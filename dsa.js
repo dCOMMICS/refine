4ae7cb371e93e1ce9d2c10ccde0ef0a88b5571f3
dCOMMICS <sanwurld@gmail.com>
2023-08-23 19:47:04 +0300

    write functioning code

--- dsa.js
+++ dsa.js
@@ -1,16 +1,17 @@
-const UserPermisions = [
+const UserPermissions = [
     'CREATE_POST', 
     'EDIT_POST',
     'READ_POST',
     'DELETE_POST',
 ];
-function createpost(postData) (
-    if (!UserPermisions.includes (
-        'CREATE_POST')){
-            return 402;
-        }
-        else{
-            return postData;
-        }
-    )
-)
+
+function createPost(postData) {
+    if (!UserPermissions.includes('CREATE_POST')) {
+        return 402; // You might want to use a more descriptive error code or message here.
+    } else {
+        // Assuming you want to return the postData if the permission is granted.
+        return postData;
+    }
+}
+
+
