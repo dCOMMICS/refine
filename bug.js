1c76c815c903b40abbda5788a0785787359777b2
dCOMMICS <sanwurld@gmail.com>
2023-08-23 19:32:14 +0300

    bug spotted

---
+++ bug.js
@@ -0,0 +1,9 @@
+const Animal = (name,type) => {
+    this.name = name;
+    this.type = type;
+    this.age = 0;
+    }
+    Animal.prototype.birthday = function () {
+        this.age++;
+    }
+    const leo = new Animal ('leo', 'lion');
